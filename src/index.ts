22d91c5bf7bc562289651bfe332295800e913490
Jared Cleghorn <jaredrcleghorn@gmail.com>
2021-12-08 08:38:34 -0500

    add Nodemailer transport

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,21 @@
 import cors from "cors"
 import dotenv from "dotenv"
 import express, { Request, Response } from "express"
+import nodemailer from "nodemailer"
+import * as SMTPTransport from "nodemailer/lib/smtp-transport"
 
 dotenv.config()
+
+const nodemailerOptions: SMTPTransport.Options = {
+  host: process.env.NODEMAILER_HOST,
+  port: parseInt(process.env.NODEMAILER_PORT as string),
+  secure: process.env.NODEMAILER_SECURE === "true",
+  auth: {
+    user: process.env.NODEMAILER_USER,
+    pass: process.env.NODEMAILER_PASS,
+  },
+}
+const transporter = nodemailer.createTransport(nodemailerOptions)
 
 const app = express()
 const port: number = 3000
