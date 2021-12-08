321f3bf2b309e45fb06e101140b74884470c3882
Jared Cleghorn <jaredrcleghorn@gmail.com>
2021-12-07 20:21:19 -0500

    use Express

--- src/index.js
+++ src/index.js
@@ -1,1 +1,10 @@
-console.log("hello, world")
+import express from "express"
+
+const app = express()
+const port = 3000
+
+app.get("/", (req, res) => res.send("hello, world"))
+
+app.listen(port, () =>
+  console.log(`jaredcleghorn.com API listening at http://localhost:${port}`)
+)
