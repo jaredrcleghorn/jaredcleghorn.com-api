0b9568c2610e51e9d0f680dcb487333d7f1317fb
Jared Cleghorn <jaredrcleghorn@gmail.com>
2021-12-08 06:52:04 -0500

    add types

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,9 @@
-import express from "express"
+import express, { Request, Response } from "express"
 
 const app = express()
-const port = 3000
+const port: number = 3000
 
-app.get("/", (req, res) => res.send("hello, world"))
+app.get("/", (req: Request, res: Response) => res.send("hello, world"))
 
 app.listen(port, () =>
   console.log(`jaredcleghorn.com API listening at http://localhost:${port}`)
